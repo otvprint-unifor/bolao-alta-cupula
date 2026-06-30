1f2157fe75bc05d5f5205b88239f39f35f0a752c
leaolives <leaolivesoficial@gmail.com>
2026-06-30 06:33:20 -0300

    brasil x japao alemanha x paraguai holanda x marrocos

---
+++ dados.js
@@ -0,0 +1,290 @@
+const dados = {
+  "participantes": [
+    {
+      "nome": "Otávio",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 0 x 2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 2 x 0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 2 x 0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2 x 1 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 0 x 2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3 x 1 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1 x 0 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2 x 0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2 x 1 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 3 x 0 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 2 x 1 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 2 x 1 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 2 x 0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 0 x 1 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 3 x 0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 2 x 1 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Eudes",
+      "pontos": 5,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 0x1 Canadá 🇨🇦", "status": "acertou-placar" },
+        { "jogo": "🇧🇷 Brasil 2x0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 1x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 0x1 Marrocos 🇲🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇨🇮 Costa do Marfim 0x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x0 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 0x1 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 1x2 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 2x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 2x0 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 0x1 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 0x1 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 3x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 1x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Gui Paiva",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 1x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 2x0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 3x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2x1 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 0x3 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x2 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x1 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x0 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 4x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 2x1 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 2x0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x1 Egito 🇪🇬(P)", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 2x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 1x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Levi Mateus",
+      "pontos": 4,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 2x1 Canadá 🇨🇦", "status": "errou" },
+        { "jogo": "🇧🇷 Brasil 3x1 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 4x1 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "(P)🇳🇱 Holanda 1x1 Marrocos 🇲🇦", "status": "acertou-placar" },
+        { "jogo": "🇨🇮 Costa do Marfim 1x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 4x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x2 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 3x2 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x1 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x1 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 1x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x2 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 1x2 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x1 Egito 🇪🇬(P)", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 3x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 3x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Paulo Rosa",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 1x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 3x1 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 2x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2x1 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 2x3 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 4x1 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 3x1 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x2 Senegal 🇸🇳(P)", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 3x1 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 2x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "(P)🇵🇹 Portugal 1x1 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 1x0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x2 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 4x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 3x2 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Ed Junior",
+      "pontos": 1,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 2x0 Canadá 🇨🇦", "status": "errou" },
+        { "jogo": "🇧🇷 Brasil 3x1 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 2x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2x1 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "(P)🇨🇮 Costa do Marfim 2x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x2 Equador 🇪🇨(P)", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 1x2 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x2 Senegal 🇸🇳(P)", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 3x1 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 2x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x3 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 1x1 Argélia 🇩🇿(P)", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 2x0 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 2x1 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 2x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Tobias",
+      "pontos": 5,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 0x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 3x0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 2x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "(P)🇳🇱 Holanda 1x1 Marrocos 🇲🇦", "status": "acertou-placar" },
+        { "jogo": "🇨🇮 Costa do Marfim 2x3 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 4x1 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 3x1 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 1x2 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 1x0 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 3x1 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 2x1 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 3x0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x1 Egito 🇪🇬(P)", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 2x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 2x1 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Leopoldino",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 0x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 3x0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 2x1 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2x1 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 1x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x1 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 3x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 1x1 Senegal 🇸🇳(P)", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 3x0 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 3x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 0x1 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 2x1 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 0x1 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 4x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 1x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Rodrigo",
+      "pontos": 5,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 1x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 1x2 Japão 🇯🇵", "status": "errou" },
+        { "jogo": "🇩🇪 Alemanha 1x1 Paraguai 🇵🇾(P)", "status": "acertou-placar" },
+        { "jogo": "🇳🇱 Holanda 0x1 Marrocos 🇲🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇨🇮 Costa do Marfim 1x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 2x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x1 Equador 🇪🇨(P)", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x0 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 3x0 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 2x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x2 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 1x0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 2x1 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 3x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 2x1 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Nicholas",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 1x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 2x0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 2x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2x0 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 0x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x0 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x0 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x0 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 2x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 2x0 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 2x0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x2 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 4x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 2x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Thiago Rocha",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 1x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 2x0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 3x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2x1 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 1x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x1 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x1 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x1 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 3x1 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 4x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 2x2 Croácia 🇭🇷(P)", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 1x0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x2 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 4x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 2x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Gabriel Pereira",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 1x2 Canadá 🇨🇦", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 2x0 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 3x1 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 2x1 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 1x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "(P)🇲🇽 México 2x2 Equador 🇪🇨", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 2x0 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "(P)🇧🇪 Bélgica 1x1 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x1 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 2x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x1 Croácia 🇭🇷(P)", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 2x0 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x2 Egito 🇪🇬", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 3x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 2x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Davi Lira",
+      "pontos": 2,
+      "palpites": [
+        { "jogo": "🇿🇦 África do Sul 1x1 Canadá 🇨🇦(P)", "status": "acertou-vencedor" },
+        { "jogo": "🇧🇷 Brasil 3x1 Japão 🇯🇵", "status": "acertou-vencedor" },
+        { "jogo": "🇩🇪 Alemanha 3x0 Paraguai 🇵🇾", "status": "errou" },
+        { "jogo": "🇳🇱 Holanda 1x0 Marrocos 🇲🇦", "status": "errou" },
+        { "jogo": "🇨🇮 Costa do Marfim 1x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇫🇷 França 3x0 Suécia 🇸🇪", "status": "pendente" },
+        { "jogo": "🇲🇽 México 0x0 Equador 🇪🇨(P)", "status": "pendente" },
+        { "jogo": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Inglaterra 3x1 RD Congo 🇨🇩", "status": "pendente" },
+        { "jogo": "🇧🇪 Bélgica 2x0 Senegal 🇸🇳", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x0 Bósnia 🇧🇦", "status": "pendente" },
+        { "jogo": "🇪🇸 Espanha 3x0 Áustria 🇦🇹", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x0 Croácia 🇭🇷", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça 2x1 Argélia 🇩🇿", "status": "pendente" },
+        { "jogo": "🇦🇺 Austrália 1x1 Egito 🇪🇬(P)", "status": "pendente" },
+        { "jogo": "🇦🇷 Argentina 3x0 Cabo Verde 🇨🇻", "status": "pendente" },
+        { "jogo": "🇨🇴 Colômbia 3x0 Gana 🇬🇭", "status": "pendente" }
+      ]
+    }
+  ]
+};
