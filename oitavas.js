a72d7173e5b61150af20dda1e5bfab9306264a46
leaolives <leaolivesoficial@gmail.com>
2026-07-03 15:41:05 -0300

    oitavas.js

---
+++ oitavas.js
@@ -0,0 +1,228 @@
+const dados = {
+  "participantes": [
+    {
+      "nome": "Eudes",
+      "pontos": 13,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá x Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai x França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil x Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México x Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal x Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos x Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Gui Paiva",
+      "pontos": 18,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 1x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 0x3 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 2x1 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x2 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 3x2 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x1 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Levi Mateus",
+      "pontos": 11,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 0x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 0x3 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 2x0 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x1 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x3 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 1x2 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Ed Junior",
+      "pontos": 7,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá x Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai x França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil x Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México x Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal x Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos x Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Tobias",
+      "pontos": 14,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá x Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai x França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil x Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México x Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal x Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos x Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Leopoldino",
+      "pontos": 15,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 0x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 1x3 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 3x2 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x2 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x2 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 1x2 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Rodrigo",
+      "pontos": 14,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá x Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai x França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil x Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México x Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal x Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos x Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Nicholas",
+      "pontos": 19,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 0x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 0x3 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 2x0 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 0x2 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 0x4 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 0x2 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Thiago Rocha",
+      "pontos": 12,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá x Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai x França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil x Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México x Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal x Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos x Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Gabriel Pereira",
+      "pontos": 16,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 0x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 0x2 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 2x0 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x1 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x2 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x1 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Pedro Jordi",
+      "pontos": 10,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 0x3 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 0x2 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 1x1 Noruega 🇳🇴(P)", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x1 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿(P)", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 2x1 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 1x2 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Otávio",
+      "pontos": 15,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 0x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 0x2 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 2x1 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 1x2 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x2 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 2x1 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Paulo Rosa",
+      "pontos": 11,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 1x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 1x2 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 3x1 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x1 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x2 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "(P)🇺🇸 Estados Unidos 1x1 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Davi Lira",
+      "pontos": 18,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá x Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai x França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil x Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México x Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal x Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos x Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Tiago Nogueira",
+      "pontos": 4,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 1x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 1x3 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 2x1 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x0 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "🇵🇹 Portugal 1x1 Espanha 🇪🇸(P)", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 1x2 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    },
+    {
+      "nome": "Bruno Oliveira",
+      "pontos": 4,
+      "palpites": [
+        { "jogo": "🇨🇦 Canadá 0x2 Marrocos 🇲🇦", "status": "pendente" },
+        { "jogo": "🇵🇾 Paraguai 1x3 França 🇫🇷", "status": "pendente" },
+        { "jogo": "🇧🇷 Brasil 3x1 Noruega 🇳🇴", "status": "pendente" },
+        { "jogo": "🇲🇽 México 2x1 Inglaterra 🏴󠁧󠁢󠁥󠁮󠁧󠁿", "status": "pendente" },
+        { "jogo": "(P)🇵🇹 Portugal 2x2 Espanha 🇪🇸", "status": "pendente" },
+        { "jogo": "🇺🇸 Estados Unidos 3x1 Bélgica 🇧🇪", "status": "pendente" },
+        { "jogo": "🇨🇭 Suíça x A DEFINIR ❓", "status": "pendente" },
+        { "jogo": "❓ A DEFINIR x A DEFINIR ❓", "status": "pendente" }
+      ]
+    }
+  ]
+};
